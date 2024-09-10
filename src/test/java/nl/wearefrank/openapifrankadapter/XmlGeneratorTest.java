65f42e6dcbd6f842b57b994ce71115c1e4ba56b6
murrok <marco.speksnijder@gmail.com>
2024-09-10 11:55:27 +0200

    Created XmlGeneratorTest class, contains tests to verify that the sender/receiver element matches the chosen option

---
+++ src/test/java/nl/wearefrank/openapifrankadapter/XmlGeneratorTest.java
@@ -0,0 +1,53 @@
+package nl.wearefrank.openapifrankadapter;
+
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+
+public class XmlGeneratorTest {
+
+    OpenapiFrankadapterApplication testApp;
+
+    @Test
+    void testXmlGeneratorReceiver() {
+        testXmlGenerator(Option.RECEIVER, "<Receiver");
+    }
+
+    @Test
+    void testXmlGeneratorSender() {
+        testXmlGenerator(Option.SENDER, "<Sender");
+    }
+
+    private void testXmlGenerator(Option templateOption, String expectedContent) {
+        InputStream inputStream = TestGetElementAttribute.class.getResourceAsStream("/TestingOASFile/openapi.json");
+        String source;
+        try {
+            source = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read the input stream", e);
+        }
+
+        SwaggerParseResult result = new OpenAPIParser().readContents(source, null, null);
+        OpenAPI openAPI = result.getOpenAPI();
+
+        try {
+            LinkedList<GenFiles> genFiles = XMLGenerator.execute(openAPI, templateOption);
+
+            genFiles.stream()
+                    .filter(genFile -> genFile.getName().endsWith(".xml"))
+                    .forEach(genFile -> {
+                        String fileContents = new String(genFile.getContent());
+                        Assertions.assertTrue(fileContents.contains(expectedContent));
+                    });
+        } catch (Exception e) {
+            Assertions.fail("Exception occurred during test: " + e.getMessage());
+        }
+    }
+}
