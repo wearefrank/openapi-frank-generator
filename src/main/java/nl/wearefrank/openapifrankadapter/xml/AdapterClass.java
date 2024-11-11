b1563d0179a9db50b333bdb05c4b0e464b407729
Marco Speksnijder <marco.speksnijder@gmail.com>
2024-11-11 14:53:02 +0100

    Changed adapter and file name to not include curly brackets

--- src/main/java/nl/wearefrank/openapifrankadapter/xml/AdapterClass.java
+++ src/main/java/nl/wearefrank/openapifrankadapter/xml/AdapterClass.java
@@ -24,7 +24,8 @@ public class AdapterClass {
 
     public AdapterClass(OpenAPI openAPI, Map.Entry<String, PathItem> path, Map.Entry<PathItem.HttpMethod, io.swagger.v3.oas.models.Operation> operation) {
         // adapterName: remove the first slash and replace the remaining slashes with -
-        this.adapterName = path.getKey().substring(1).replace("/", "-") + "-" + operation.getKey();
+        String tempAdapterName = path.getKey().substring(1).replace("/", "-") + "-" + operation.getKey();
+        this.adapterName = tempAdapterName.replace("{", "").replace("}", "");
         this.adapterDescription = openAPI.getInfo().getDescription();
     }
 
